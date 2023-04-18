a86f3d1f06f59b1dfb99bedb235a55bffb56ad0c
SayyedAhamd <130682645+SayyedAhamd@users.noreply.github.com>
2023-04-18 22:56:29 +0330

    Update run.bat

--- run/run.bat
+++ run/run.bat
@@ -1,2 +1,6 @@
-java --module-path path\to\javafx-sdk-20\lib --add-modules javafx.controls,javafx.fxml,javafx.swing -jar rubik.jar
+set PATH_TO_FX="C:\Ahmad\Java\tools\javafx\openjfx-20\javafx-sdk-20\lib"
+cd ..
+set PATH_TO_BIN=%cd%\bin
+javac --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml,javafx.swing -d %PATH_TO_BIN% src\hellofx\*.java
 
+java --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml,javafx.swing -cp %PATH_TO_BIN% hellofx.Main
